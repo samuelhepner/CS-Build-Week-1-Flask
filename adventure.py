a91d2f8200374fba8d47ae11181c81d1873ca50a
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2020-02-07 12:18:20 -0500

    allowing all headers

--- adventure.py
+++ adventure.py
@@ -28,7 +28,7 @@ app = Flask(__name__)
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,Origin')
+    response.headers.add('Access-Control-Allow-Headers', '*')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
 
